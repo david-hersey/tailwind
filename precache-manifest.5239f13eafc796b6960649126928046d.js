fa0583558dea78fd7a6291b37939f84cc6914a0e
Hersey <hersey@herseys-imac.home>
2020-09-29 10:47:03 +0100

    Updates

---
+++ precache-manifest.5239f13eafc796b6960649126928046d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae8d0bfcb8716c911fdc642a757ea23",
+    "url": "/tailwind/index.html"
+  },
+  {
+    "revision": "4cffe4076883b3611b3b",
+    "url": "/tailwind/static/css/main.4c8f7b31.chunk.css"
+  },
+  {
+    "revision": "c226e27877e561fe9e50",
+    "url": "/tailwind/static/js/2.13f428ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailwind/static/js/2.13f428ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cffe4076883b3611b3b",
+    "url": "/tailwind/static/js/main.91985095.chunk.js"
+  },
+  {
+    "revision": "b78443f1d104463a4d8d",
+    "url": "/tailwind/static/js/runtime-main.b3545ee0.js"
+  },
+  {
+    "revision": "cfc690b34cd1dc9d5b4ac50999d493d2",
+    "url": "/tailwind/static/media/dj.cfc690b3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tailwind/static/media/logo.5d5d9eef.svg"
+  }
+]);
